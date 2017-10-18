5ab7633da4ac0e06bb3defb77223a59dbc56e5b9
Brian Williams <williamsbm2000@comcast.net>
2017-10-18 12:10:06 -0700

    added static methods for passenger handling

--- shared.py
+++ shared.py
@@ -23,7 +23,7 @@ def on_disconnect(client, userdata, rc):
 	print "disconnected in a normal way"
 
 def on_log(client, userdata, level, buf):
-	print "log: {}".format(buf)
+	print "log: {}".format(buif)
 
 mqtt_client.on_connect = on_connect
 mqtt_client.on_disconnect = on_disconnect
@@ -36,4 +36,34 @@ class Passenger():
 		self.id = id
 
 	def __repr__(self):
-		return "Passenger (%d)" % self.id
+		return "Passenger #%d" % self.id
+
+	@staticmethod
+	def from_string(text):
+		if "Passenger" in text:
+			pid = int(text[11:])
+			print pid
+			return Passenger(pid)
+
+	@staticmethod
+	def list_from_string(text):
+		passengers = []
+		text = text[1:-1]
+		split_text = str.split(text, ",")
+		for p in split_text:
+			p = p.strip()
+			passengers.append(Passenger.from_string(p))
+		return passengers
+
+
+#test = []
+#test.append(Passenger(3))
+#test.append(Passenger(5))
+#test.append(Passenger(121))
+
+#text = str(test)
+#print(text)
+
+#new_test = Passenger.list_from_string(text)
+
+#print(new_test)
